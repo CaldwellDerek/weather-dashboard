b40bf85b804bdfc388de16dd9f44ba388b1646fe
Derek Caldwell <djohncaldwell@gmail.com>
2023-01-09 17:35:13 -0800

    search results and forecast sections added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
 let leftSide = document.querySelector('.left-side');
+let searchField = document.querySelector('.search-field');
+let searchButton = document.querySelector('.search-btn');
+
+searchButton.addEventListener('click', ()=> {
+    let text = searchField.value;
+    let searchResult = document.createElement('button');
+    searchResult.textContent = text;
+    leftSide.append(searchResult);
+})
